878f1537535649c8dca83a9c409fba0a195e31de
rahach <rhachmeier@gmail.com>
2019-01-29 16:35:16 +0100

    first commit

--- Server/config/app.php
+++ Server/config/app.php
@@ -165,8 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Superbalist\LaravelGoogleCloudStorage\GoogleCloudStorageServiceProvider::class,
-        Barryvdh\Cors\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -224,7 +223,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'GCS' => Superbalist\LaravelGoogleCloudStorage\GoogleCloudStorageServiceProvider::class
 
     ],
 
