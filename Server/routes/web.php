a1adec2ddb230ef461686607cad9c9f8f80f0edf
rahach <45273068+rahach@users.noreply.github.com>
2019-03-07 07:52:25 +0100

    Update web.php

--- Server/routes/web.php
+++ Server/routes/web.php
@@ -23,11 +23,6 @@
 
 */
 
-// Route::resource('products', 'ProductsController');
-//Route::resource('categories', 'CategoriesController');
-// Route::get('/categories/{id}/products', 'CategoriesController@products');
-// Route::get('/categories/{id}/parent', 'CategoriesController@parent');
-// Route::get('/categories/{id}/children', 'CategoriesController@children');
 
 Route::get('/category/{category_slug}', 'CategoriesController@show');
 Route::get('/categories', 'CategoriesController@index');
@@ -35,18 +30,9 @@ Route::get('/supplier/{supplier_slug}', 'SuppliersController@show');
 Route::get('/advertiser/{advertiser_slug}', 'AdvertisersController@show');
 Route::get('/{product_slug}', 'ProductsController@show');
 
-// Route::resource('suppliers', 'SuppliersController');
-// Route::resource('attributes', 'AttributesController');
+
 
 Route::get('/', 'PagesController@home');
 Route::get('/contact', 'PagesController@contact');
 Route::get('/about', 'PagesController@about');
 
-
-// Route::get('/categories', 'CategoriesController@index');
-//Route::get('/advertisers', 'AdvertisersController@index');
-//Route::get('/suppliers', 'SuppliersController@index');
-
-// Route::group(['prefix' => 'admin'], function () {
-
-// });
